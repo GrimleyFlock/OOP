9a1a831eed503c50c2e8cd0e398df7a1c3683451
GrimleyFlock <grimleyflock@gmail.com>
2019-01-18 15:22:54 +0300

    Delete function added

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -7,21 +7,24 @@ class student{
 	string name;
 	int i, student_1;
 	int search_number;
+	int s;
+	
 	
 	public:
 		void insert(){
+			s = 5;
 			cout<<"Enter name: "<<endl;
 			cin>>name;
 			cout<<"Insert the 5 scores:"<<endl;
 			
-			for( i = 0; i < 5; i ++){
+			for( i = 0; i < s; i ++){
 				cin>>score[i];
 			}
 		}
 		
 		void display(){
 			cout<<"Your name is: "<<name<<" and your scores are: "<<endl;
-			for( i = 0; i < 5; i++){
+			for( i = 0; i < s; i++){
 				cout<<score[i]<<endl;
 			}
 		}
@@ -29,17 +32,40 @@ class student{
 		int search(){
 			cout<<"Key in number to be searched: "<<endl;
 			cin>>search_number;
-			for( i = 0; i < 5; i++){
+			for( i = 0; i < s; i++){
 				if ( score[i] == search_number){
 					cout<<"Number Found!"<<endl;
 					return 0;
 				} 
 				}
 			}
+			
+		int num_delete(){
+			int delete_number, i;
+			cout<<"Key in number to be deleted: "<<endl;
+			cin>>delete_number;
+			
+			for ( i = 0; i < s; i++){
+				if (score[i] == delete_number){
+					cout<<"Number Found"<<endl;
+					break;
+				}
+			}
+				if ( i == 5){
+				cout<<"Not Found"<<endl;
+				return 0;
+			} else {
+				for ( int j = i; j < 5; j++){
+					score[j] = score[j+1];
+				}	
+			}
+			s--;
+				return 0;
+			}
 		
-		}
+	};
 	
-};
+
 
 
 
@@ -50,8 +76,8 @@ int main() {
 	student_1.insert();
 	student_1.display();
 	
-	student_1.search();
-
+	student_1.num_delete();
+	student_1.display();
 	
 	return 0;
 }
